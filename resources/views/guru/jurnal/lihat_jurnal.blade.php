f47ced243fce6c4a1fa6559a0a6d785c8253706c
gustifa <fauzangustifa@gmail.com>
2025-02-18 14:41:15 +0700

    Update Bakend

--- resources/views/guru/jurnal/lihat_jurnal.blade.php
+++ resources/views/guru/jurnal/lihat_jurnal.blade.php
@@ -5,7 +5,7 @@
    Jadwal Pelajaran
 @endsection
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> --}}
 <style>
     .large-chexbox{
         transform: scale(1.5);
